4c0843336133756acef2e9f19291d74ae603e153
Shakthipooja <shakthipoojah@gmail.com>
2026-06-30 16:16:26 +0530

    Implement patient creation and display functionality
    Added functionality to create and display multiple patient objects.

--- hospital management.py
+++ hospital management.py
@@ -19,8 +19,14 @@ class Patient:
         print("Age:", self.age)
         print("Disease:", self.disease)
         print("Blood Group:", self.blood_group)
+        print()
 
 
+# Patient database
+patients = []
+
+
+# Create patient objects
 patient1 = Patient(
     "P001",
     "Ramesh",
@@ -29,4 +35,20 @@ patient1 = Patient(
     "O+"
 )
 
-patient1.display()
+patient2 = Patient(
+    "P002",
+    "Priya",
+    28,
+    "Asthma",
+    "B+"
+)
+
+
+# Store patients in list
+patients.append(patient1)
+patients.append(patient2)
+
+
+# Display all patients
+for patient in patients:
+    patient.display()
